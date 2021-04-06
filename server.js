4ce253aae96e2c42f99a1c1f44f3ba8eec3ee739
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-04-06 21:29:50 +0000

    create, read and read by id

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const {uuid} = require('uuidv4')
 
 const server = express();
+server.use(express.json());
+
+contatos = [];
 
 server.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.json(contatos);
 })
 
-server.listen(process.env.PORT || 3000);
+server.get('/:id', function(request, response) {
+    const id = request.params.id;
+    response.json(id);
+})
+
+server.post('/', function(request, response) {
+    const nome = request.body.nome;
+    const telefone = request.body.telefone
+
+    contatos = {
+        id: uuid(),
+        nome,
+        telefone,
+    };
+
+    response.push(contatos);
+
+    response.status(201).send();
+})
+
+server.listen(process.env.PORT || 3000, () => console.log("Server is running!"));
