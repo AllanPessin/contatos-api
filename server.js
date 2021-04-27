e7eb49a0c3d48032ed244d932ce8fa1b1453656f
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-04-27 18:28:20 +0000

    new methods, update anda delete

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ server.get('/', async function(request, response) {
     response.json(contatos);
 })
 
-server.get('/:id', function(request, response) {
+server.get('/:id', async function(request, response) {
     const id = request.params.id;
     const result = contatos.filter(contato => contato.id == id)
     response.json(result);
 })
 
-server.post('/', function(request, response) {
+server.post('/', async function(request, response) {
     const nome = request.body.nome;
     const telefone = request.body.telefone;
 
@@ -25,6 +25,22 @@ server.post('/', function(request, response) {
 
     contatos.push(contato);
     response.status(201).send();
+})
+
+server.put('/:id', async function(request, response) {
+    const id = request.params.id;
+    const nome = request.params.nome;
+    const telefone = request.params.telefone;
+
+    const result = await database.update(id, nome, telefone);
+    response.status(200).send();
+})
+
+server.delete('/:id', async function(request, response ){
+    const id = request.params.id;
+
+    const result = await database.delete(id);
+    response.status(200).send();
 })
 
 server.listen(process.env.PORT || 3000, () => console.log("Server is running!"));
